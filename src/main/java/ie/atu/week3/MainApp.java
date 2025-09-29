526f76b2054f4c60cc2a0fafeb29d970ed33c543
SuperButler <sethb2k4@gmail.com>
2025-09-29 22:32:00 +0100

    subtract two number sub(int,int)

--- src/main/java/ie/atu/week3/MainApp.java
+++ src/main/java/ie/atu/week3/MainApp.java
@@ -7,11 +7,15 @@ public class MainApp {
         Scanner sc = new Scanner(System.in);
         // Create an instance of Calculator
         Calculator calc = new Calculator();
-        System.out.println("Simple Calculator - Add Two Numbers");
+
+        System.out.println("Simple Calculator");
+
         System.out.print("Enter first number: ");
         int a = sc.nextInt();
+
         System.out.print("Enter second number: ");
         int b = sc.nextInt();
+
         int result = calc.add(a, b);
         System.out.println("Result: " + result);
         sc.close();
